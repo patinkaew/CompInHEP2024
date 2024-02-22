769eb9aabf048e5fa197e07ad806be4fcc9136ad
Inkaew <pinkaew@dhcp-hip-8.mobile.helsinki.fi>
2024-02-22 18:18:50 +0200

    submit Ex5

---
+++ Exercises/Ex5/pinkaew/plot_norm.cxx
@@ -0,0 +1,42 @@
+void plot_norm(){
+    TFile *in_file = TFile::Open("out.root", "READ");
+    TTree* t = (TTree*)in_file->Get("tree");
+
+    Double_t s;
+    t -> SetBranchAddress("normal",&s);
+
+    TH1D* h= new TH1D("histo","", 50, -5, 5);
+
+    for(size_t i=0; i < t -> GetEntries(); i++){
+        t -> GetEntry(i);
+        h -> Fill(s);
+    }
+
+    TF1 *fn = new TF1("fn", "gaus", -5, 5);
+    h -> Fit("fn");
+
+    h -> SetFillColor(kYellow);
+    h -> SetLineWidth(3);
+
+    fn -> SetLineColor(kBlack);
+    fn -> SetLineWidth(3);
+
+    h -> GetXaxis() -> SetTitle("Random number");
+    h -> GetYaxis() -> SetTitle("Number of events");
+    
+    TCanvas* canvas = new TCanvas("fit","",500,500);
+    canvas->cd();
+
+    h -> Draw();
+    fn -> Draw("SAME");
+
+    canvas -> Print("plot.pdf");
+
+    TFile *out_file = TFile::Open("histo.root", "RECREATE");
+    out_file -> cd();
+    h -> Write();
+    out_file -> Close();
+    in_file -> Close();
+
+    gApplication->Terminate();
+}
---
+++ Exercises/Ex5/pinkaew/random_norm.cxx
@@ -0,0 +1,22 @@
+void random_norm(){
+    size_t N = 1000;
+    UInt_t seed = 5679;
+    TRandom3 *generator = new TRandom3(seed);
+
+    TFile *f = TFile::Open("out.root", "RECREATE");
+    f -> cd();
+
+    TTree * t = new TTree("tree", "title");
+    Double_t s;
+    t -> Branch("normal", &s, "normal/D");
+
+    for(size_t i = 0; i < N; i++){
+        s = generator -> Gaus();
+        t -> Fill();
+    }
+
+    t -> Write();
+    f -> Close();
+
+    exit(0);
+}
